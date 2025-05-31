f2ded54d7fdc9648721d47f34c61948f01811def
Nikitas Menounos <menounosnikitas@gmail.com>
2025-05-31 18:03:48 +0300

    Update configuration

--- configuration.nix
+++ configuration.nix
@@ -105,21 +105,30 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-     vscode
-     opera
-     git
-     spotify
+
+    # --- Essential Tools ---
+    git           # Version control
+    direnv        # Environment variable management, integrates well with VS Code
+    # wget        # Optional: CLI tool for downloading files
+    vim         # Optional: Editor for editing configuration.nix
+
+    # --- Development Environment ---
+    vscode        # Visual Studio Code editor
+    docker        # Docker Container Application 
+    # --- Browsers ---
+    opera         # Web browser
+
+    # --- Media ---
+    spotify       # Music streaming client
   ];
 
-  programs.git = {
-    enable = true;
-    config = {
-        user.name = "nikmen";
+  programs.git = { 
+    enable = true; 
+    config = {  
+      user.name = "nikitasmen";
     	user.email = "menounosnikitas@gmail.com";
     };
-  };
+  }; 
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
