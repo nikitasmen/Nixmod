a156f592bb0c0cce9f785e7c78b6a8df0f20032e
nikitasmen <menounosnikitas@gmail.com>
2025-06-08 23:25:57 +0300

    Add stable version on production

--- configuration.nix
+++ configuration.nix
@@ -2,18 +2,25 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib,  ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./nvidia-configuration.nix
     ];
 
+  # Use flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.enable = false;
+  #boot.loader.grub.device = "/dev/nvme0n1";
+  #boot.loader.grub.useOSProber = true;
+  
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -34,7 +41,7 @@
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "el_GR.UTF-8";
     LC_IDENTIFICATION = "el_GR.UTF-8";
-    LC_MEASUREMENT = "el_GR.UTF-8";
+    LC_MEASUREMENT = "el_GR.UTF-8"; 
     LC_MONETARY = "el_GR.UTF-8";
     LC_NAME = "el_GR.UTF-8";
     LC_NUMERIC = "el_GR.UTF-8";
@@ -48,7 +55,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -99,67 +106,39 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # Enable hyprland 
+  programs.hyprland.enable = true; 
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-
+ 
     # --- Essential Tools ---
-    tmux          # Terminal Multiplexer
     git           # Version control
-    direnv        # Environment variable management, integrates well with VS Code
+    #direnv        # Environment variable management, integrates well with VS Code
+    tmux        # terminal multiplexer
+    tree        # structured dir displayer  
     # wget        # Optional: CLI tool for downloading files
-    vim           # Optional: Editor for editing configuration.nix
-    helix         # Text Editor 
-
+    vim         # Optional: Editor for editing configuration.nix
+    helix       # Editor 
+    alacritty 
+    nnn         # File explorer    
     # --- Development Environment ---
-    vscode        # Visual Studio Code editor
+    #vscode        # Visual Studio Code editor
     docker        # Docker Container Application 
     # --- Browsers ---
     opera         # Web browser
 
-    # --- Programmin stuff --- 
-    nodejs
-    playwright-driver
+    kitty        # Hyprland terminal emulator. Required for default config  
+    waybar       # Display Server
+    # rofi-wayland
+    wofi         # Search bar 
     # --- Media ---
     spotify       # Music streaming client
-
-    # --- Communication --- 
-    slack
-    discord-ptb     # Discord official client  
-    # legcord       # Discord alternative UNOFFICIAL CLIENT
-
-    # --- Dependencies --- 
-
   ];
-
-#  programs.helix = {
-#   enable = true;
-#   settings = {
-#     theme = "autumn_night_transparent";
-#     editor.cursor-shape = {
-#       normal = "block";
-#       insert = "bar";
-#       select = "underline";
-#     };
-#   };
-#   languages.language = [{
-#     name = "nix";
-#     auto-format = true;
-#     formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-#   }];
-#   themes = {
-#     autumn_night_transparent = {
-#       "inherits" = "autumn_night";
-#       "ui.background" = { };
-#     };
-#   };
-#  };
-  #users.users.nikitasmen.packages = with pkgs; [ 
-    
-  #]
 
   programs.git = { 
     enable = true; 
@@ -196,4 +175,4 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
-}
+} 
