ace6dd04d09f6e656185923758fbc17cd6844861
nikitasmen <menounosnikitas@gmail.com>
2025-06-10 18:17:11 +0300

    Update battery thresholds

--- configuration.nix
+++ configuration.nix
@@ -35,7 +35,7 @@
 
        #Optional helps save long term battery health
        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+       STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
 
       };
   };
