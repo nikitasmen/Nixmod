0ef356a380c7cf0de61e33c6cf958f7607ffe8bd
nikmen <menounosnikitas@gmail.com>
2025-07-04 13:27:52 +0300

    refactor(unixKit): Install via github

--- unixkit.nix
+++ unixkit.nix
@@ -1,13 +1,38 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  unixKitScripts = pkgs.runCommandLocal "unixkit" {} ''
-    mkdir -p $out/bin
-    cp -r ${./scripts}/* $out/bin/
-    chmod -R +x $out/bin
-  '';
+  # Fetch the latest commit from GitHub repo
+  unixKitSrc = builtins.fetchGit {
+    url = "https://github.com/nikitasmen/UnixKit.git";
+    # Omitting 'rev' to always get the latest commit on rebuild
+  };
+
+  # Build the package
+  unixKit = pkgs.stdenv.mkDerivation {
+    name = "unixkit";
+    version = "unstable-${builtins.substring 0 8 unixKitSrc.rev}";
+    
+    src = unixKitSrc;
+    
+    # No build phase needed if these are just scripts
+    dontBuild = true;
+    
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r $src/* $out/bin/
+      chmod -R +x $out/bin
+    '';
+    
+    meta = with lib; {
+      description = "Collection of Unix utility scripts";
+      homepage = "https://github.com/nikitasmen/UnixKit";
+      license = licenses.mit;  # Adjust according to the actual license
+      platforms = platforms.all;
+      maintainers = [ "nikitasmen" ];
+    };
+  };
 in {
   environment.systemPackages = [
-    unixKitScripts
+    unixKit
   ];
 }
