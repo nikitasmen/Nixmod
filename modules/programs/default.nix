663c3b9c088975db07b399cf3466c994823eeeb2
nikmen <menounosnikitas@gmail.com>
2025-09-15 11:57:28 +0300

    Remove deprecated NixMod configuration files
    - Deleted the default module files for system, desktop, and programs, including specific configurations for audio, boot, fonts, locale, networking, power, and user management.
    - Removed individual module files for Hyprland, terminal emulators, and various applications.
    - Cleaned up the README documentation as part of the restructuring process.

--- modules/programs/default.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  imports = [
-    ./applications.nix
-    ./development.nix
-  ];
-}
--- modules/system/fonts.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  # Fonts configuration
-  fonts.packages = with pkgs; [
-    pkgs.nerd-fonts.fira-code
-    pkgs.nerd-fonts.jetbrains-mono
-  ];
-}
