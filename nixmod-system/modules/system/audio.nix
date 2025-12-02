ca5529d50b74afcdaf3a72f70d29f92334e9a83e
nikitasmen <menounosnikitas@gmail.com>
2025-12-02 14:13:30 +0200

    Add obs  and plugins

--- nixmod-system/modules/system/audio.nix
+++ nixmod-system/modules/system/audio.nix
@@ -9,15 +9,36 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true; 
   };
 
+  # Enable OBS
+  programs.obs-studio = {
+    enable = true;
+
+    # Optional! Nvidia Hardware acceleration 
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true; 
+      }
+    ); 
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-gstreamer
+      obs-vkcapture
+    ];    
+  };
+  
   # Audio related packages
   environment.systemPackages = with pkgs; [
     pavucontrol  # Sound control panel
     playerctl    # Music player controller
     cava         # Audio Visualizer
+    obs-studio   # OBS studio 
   ];
   
   # Bluetooth configuration
