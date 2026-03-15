278641924e00621b9b19f989404b9da11731a3d1
nikmen <menounosnikitas@gmail.com>
2026-03-15 17:16:46 +0200

    Add Neovim configuration files including options, keymaps, and plugin setup

---
+++ nixmod-dotfiles/nvim/lua/options.lua
@@ -0,0 +1,20 @@
+-- Neovim options
+-- Disable netrw (using neo-tree)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.wrap = false
+vim.opt.breakindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.clipboard = "unnamedplus"
