fe7f068502df476032b7919b717b80eaa04215f8
nikitasmen <menounosnikitas@gmail.com>
2026-03-15 16:57:43 +0200

    Optimize nvidia with hyprland laptop

--- nixmod-system/nvidia-configuration.nix
+++ nixmod-system/nvidia-configuration.nix
@@ -1,21 +1,27 @@
 { config, pkgs, lib,  ... }:
 
 {
+   
   #Enable OpenGl
   hardware.graphics = {
   enable = true; 
   }; 
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
-
+    powerManagement.enable = true;
+    powerManagement.finegrained = true; 
+    
     open = true;
     
     prime = {
       intelBusId = "PCI:5:0:0";
       nvidiaBusId = "PCI:1:0:0";
+      offload.enable = true; 
+      offload.enableOffloadCmd = true; 
     };
+    package = config.boot.kernelPackages.nvidiaPackages.stable; 
   };
-}
+} 
