179fdddffa3cc1331775feea688f9386c6a4cf8c
nikitasmen <menounosnikitas@gmail.com>
2026-09-11 15:07:18 +0300

    Add video and audio group

--- nixmod-system/modules/users/nikmen.nix
+++ nixmod-system/modules/users/nikmen.nix
@@ -8,7 +8,7 @@
   users.users.nikmen = {
     isNormalUser = true;
     description = "nikmen";
-    extraGroups = [ "networkmanager" "wheel" "docker" "input" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "video" "audio" "input" ];
     packages = with pkgs; [
       slack   # Communication
       viber   # Communication
